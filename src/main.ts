7791b35baa75826c90f78413453cc4097b234b09
Nuel Nonaxce <agsisasleep@gmail.com>
2025-04-01 15:12:40 +0800

    vroom

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,4 @@ import { k } from "./engine"    //  get kaplay context
 import "./loader";              // load all assets on start
 
 k.loadRoot("./");
-k.loadSprite("bean", "sprites/bean.png");
 
-k.add([k.pos(120, 80), k.sprite("bean")]);
-
-k.onClick(() => k.addKaboom(k.mousePos()));
