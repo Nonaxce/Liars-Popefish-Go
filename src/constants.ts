1cb6d9cd6c3ea1f77e1e4ee139b6aba2049053dc
Nuel Nonaxce <agsisasleep@gmail.com>
2025-04-25 21:03:05 +0800

    ioogyug

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 
 export const NUM_OF_JOKERS : number = 2;
 
-export const MAX_PLAYERS : number = 4 ;
+export const MAX_ROOM_CAPACITY : number = 4 ;
 
 export const CARD_POS_Y : number = 40
--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { config } from "./config";           // get firebase api config
 import { k } from "./engine";                // get kaplayjs context
 import "./loader";                           // load all assets on start
 import { log, byteSize, deckStringToArray, deckArrayToString } from "./utils";                        
+import { MAX_ROOM_CAPACITY } from "./constants";
 
 // Firebase sdk functions
 import { initializeApp } from "firebase/app";
@@ -20,25 +21,39 @@ const database = getDatabase(app);
 
 
 (function() {
+
     onAuthStateChanged(auth, (user) => {
-
-
         if (user.isAnonymous && user) {
+            log("signed in");
 
+            const playerId : string = user.uid;
+            const playerRef = ref(database, `players/${playerId}`);
+
+            set(playerRef, {
+                roomID: "",
+                hand: [],
+                name: Math.floor(Math.random() * 1000),
+                isTurn: false,
+                isInGame: false
+            })
+
+            onDisconnect(playerRef).remove()
         } else {
-
+            log("logged out");
         }
-
-        // events when user signs in anonymously
-        signInAnonymously(auth).then(() => {
-
-        }).catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-
-            console.error(errorCode, errorMessage)
-        })
     })
+
+    // events when user signs in anonymously
+    signInAnonymously(auth).then(() => {
+        log("signed in anon");
+    }).catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage)
+    })
+
+
+
 })()
 
 
