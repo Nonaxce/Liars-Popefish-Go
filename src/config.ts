2c7287bc3e03683bcc9a34e4149b9b4e036f6ffe
Nuel Nonaxce <agsisasleep@gmail.com>
2025-04-28 13:36:51 +0800

    VERY LAST COMMIT SINCE

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 export const config = {
-    apiKey: "AIzaSyC8bbqqqeZP2u7iU9t1FxmwwYCGPzn5AG0",
-    authDomain: "lpfg-devtest.firebaseapp.com",
-    databaseURL: "https://lpfg-devtest-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "lpfg-devtest",
-    storageBucket: "lpfg-devtest.firebasestorage.app",
-    messagingSenderId: "1038529881681",
-    appId: "1:1038529881681:web:eb328803403ddef504fa70",
-    measurementId: "G-6MBNDZHN28"
+    apiKey: "XXXXXXXXXXXXXXXXXXXX",
+    authDomain: "XXXXXXXXXXXXXXXXXXXXX",
+    databaseURL: "XXXXXXXXXXXXXXXXXXXXXXXXXX",
+    projectId: "XXXXXXXXXXXXXXXXX",
+    storageBucket: "XXXXXXXXXXXXXXXXXXXXXXXXXX",
+    messagingSenderId: "XXXXXXXXXXXXXXXXX",
+    appId: "X:XXXXXXXXXXXXXXXXXXXXXXX",
+    measurementId: "X-XXXXXXXXXXX"
 }
