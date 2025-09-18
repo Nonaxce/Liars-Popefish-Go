4c3e26275d076c6bfd85b132e8eabf2a63d40099
Nuel Nonaxce <agsisasleep@gmail.com>
2025-09-18 18:01:05 +0800

    REVAMPED A LOT

---
+++ src/kaplay.ts
@@ -0,0 +1,8 @@
+import kaplay from "kaplay";
+
+export const k = kaplay({
+    width: 1280,
+    height: 720,
+    maxFPS: 120,
+    letterbox: true,
+})
--- src/main.ts
+++ src/main.ts
@@ -1,54 +1,16 @@
-import { k } from "./engine";                // get kaplayjs context
-import "./loader";                           // load all assets on start
+import { k } from "./kaplay";
+import "./loader.js";
+import "./scenes/menu";
 
 
-import { addButton } from "./ui/buttons";   // import ui components
-
-
-
-// temp
-const cards: string[] = ["sa", "s1", "sk", "s0"];
-
-
-// game
-k.scene("#game", () => {
-    const handArea = k.add([
-        k.pos(k.center().x, k.center().y - 100),
-    ])
-
-    let posX: number = 0
-    for(const card of cards) {
-        handArea.add([
-            
-        ])
-    }
-    
+k.onLoad(() => {
+    k.go("menu");
 })
 
-// menu
-k.scene("#menu", () => {
-
-    //k.setBackground(k.BLACK);
-
-    const playButton = addButton(k.center().x, k.center().y, "hearts_ace", "play");
-    k.debug.log(playButton.height)
-    playButton.onClick(() => {
-        k.go("#game")
-    }, "left");
-
-    const a = k.add([
-        k.pos(k.center()),
-        k.sprite("spades_king"),
-    ])   
-
-    console.log(a.width)
-});
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-    k.go("#menu")
-})
+
 
 
 
